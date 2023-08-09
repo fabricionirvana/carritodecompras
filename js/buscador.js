02421a4949127c582f40f54b37543a0f0766d23b
fabricionirvana <fdhnirvana@gmail.com>
2023-08-09 16:56:15 -0300

    2.0

---
+++ js/buscador.js
@@ -0,0 +1,28 @@
+
+
+  
+
+
+/*BUSCADOR */
+document.addEventListener ("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+        if (e.key ==="Escape")e.target.value = ""
+        document.querySelectorAll(".articulos") .forEach (Bebida =>{
+            Bebida.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+           ?Bebida.classList.remove("filtro")
+           :Bebida.classList.add("filtro")
+        }
+            )
+    }
+e.target.matches ("#buscador")
+
+    /*console.log(e.target.value)*/
+    
+})
+
+/*Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'No se encontraron resultados!',
+  })*/
--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-/*BUSCADOR */
-document.addEventListener ("keyup", e=>{
 
-    if (e.target.matches("#buscador")){
-        if (e.key ==="Escape")e.target.value = ""
-        document.querySelectorAll(".articulos") .forEach (Bebida =>{
-            Bebida.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-           ?Bebida.classList.remove("filtro")
-           :Bebida.classList.add("filtro")
-        }
-            )
-    }
-e.target.matches ("#buscador")
-
-    /*console.log(e.target.value)*/
-})
 /*Swal.fire({
     icon: 'error',
     title: 'Oops...',
